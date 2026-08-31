d0acb311a7f8e942324bf3373b9a9f0da835c426
Peter-mutiso <peterlynmtapeli98@gmail.com>
2026-08-31 14:38:38 +0300

    Solved price to kes

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,4 +1,4 @@
 export { Money, CurrencyMismatchError, InvalidDecimalStringError, NonIntegerAmountError } from './money';
 export type { RoundingMode } from './money';
-export { USD, EUR, GBP, KNOWN_CURRENCIES } from './currency';
+export { KES, USD, KNOWN_CURRENCIES } from './currency';
 export type { Currency } from './currency';
